0062e7da70c8dcfa310d3d60d16c1c33918d9931
Moritz Warning <moritzwarning@web.de>
2019-01-10 21:26:34 +0100

    code style change

--- server.c
+++ server.c
@@ -2,48 +2,45 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+
 #include "ipc.h"
 
+
 int main() {
-	int fd;
 	struct sockaddr_un addr;
-	int ret;
-	char buff[8192];
 	struct sockaddr_un from;
-	int ok = 1;
-	int len;
 	socklen_t fromlen = sizeof(from);
+	char buf[8192];
+	int ret;
+	int len;
+	int fd;
 
 	if ((fd = socket(PF_UNIX, SOCK_DGRAM, 0)) < 0) {
-		perror("socket");
-		ok = 0;
+		perror("socket()");
+		return 1;
 	}
 
-	if (ok) {
-		memset(&addr, 0, sizeof(addr));
-		addr.sun_family = AF_UNIX;
-		strcpy(addr.sun_path, SERVER_SOCK_FILE);
-		unlink(SERVER_SOCK_FILE);
-		if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-			perror("bind");
-			ok = 0;
-		}
+	memset(&addr, 0, sizeof(addr));
+	addr.sun_family = AF_UNIX;
+	strcpy(addr.sun_path, SERVER_SOCK_FILE);
+	unlink(SERVER_SOCK_FILE);
+	if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		close(fd);
+		perror("bind()");
+		return 1;
 	}
 
-	while ((len = recvfrom(fd, buff, 8192, 0, (struct sockaddr *)&from, &fromlen)) > 0) {
-		printf ("recvfrom: %s\n", buff);
-		strcpy (buff, "transmit good!");
-		ret = sendto(fd, buff, strlen(buff)+1, 0, (struct sockaddr *)&from, fromlen);
+	while ((len = recvfrom(fd, buf, sizeof(buf), 0, (struct sockaddr *)&from, &fromlen)) > 0) {
+		printf("received %d bytes: %s\n", len, buf);
+		strcpy(buf, "transmit good!");
+		ret = sendto(fd, buf, strlen(buf)+1, 0, (struct sockaddr *)&from, fromlen);
 		if (ret < 0) {
-			perror("sendto");
+			perror("sendto()");
 			break;
 		}
 	}
-	
 
-	if (fd >= 0) {
-		close(fd);
-	}
+	close(fd);
 
 	return 0;
 }
